c842da9a3e705fb89334a23a010e256cf8908a29
ShmoopySoft <info@shmoopysoft.co.za>
2020-02-08 18:39:38 +0200

    Just a commit test from VS 2019.

---
+++ ShmoopySoft Hello World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShmoopySoft_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey(true);
+        }
+    }
+}
